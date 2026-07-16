9c491ceacf5a82c7065b987c91fd79fea28ceac3
Iqra Aslam <amalmuzamal42@gmail.com>
2026-07-16 19:04:37 +0500

    Added index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        this is my first php website iqra
+        <?php
+        echo"Hello world and this is printed using php";
+        print"hello iqra aslam this is printed using php "; 
+         echo"Hello  this is printed using php";
+        print"hello  this is printed using php "; 
+         echo"Hello  this is printed using php";
+        print"hello  this is printed using php "; 
+         echo"Hello  this is printed using php";
+        print"hello  this is printed using php "; 
+         echo"Hello  this is printed using php";
+        print"hello  this is printed using php "; 
+         echo"Hello  this is printed using php";
+        print"hello  this is printed using php "; 
+        ?>
+     
+    </div>
+
+
+</body>
+</html>
